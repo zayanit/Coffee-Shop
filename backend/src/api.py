d875f716b7ffeabdbf5defe8d6331e6b40239e71
Mohammed Zayan <zayanit@gmail.com>
2021-08-10 22:00:36 +0300

    Initialize the database

--- backend/src/api.py
+++ backend/src/api.py
@@ -11,13 +11,7 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-!! Running this funciton will add one
-'''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 '''
