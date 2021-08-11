97242dc22e03d0a9a2cc288d50a381b15d4b3340
Mohammed Zayan <zayanit@gmail.com>
2021-08-11 19:58:49 +0300

    Auth0 frontend configuration

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-nevfm7ge', // the auth0 domain prefix
+    audience: 'coffe', // the audience set for the auth0 app
+    clientId: 'MB6k28kQhkQGQRPT8sIq7ezrs18g4saV', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
